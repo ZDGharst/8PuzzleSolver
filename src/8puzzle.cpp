233a28be753483682c95de9b6d2e2686848bc938
zach.gharst <zdgharst@gmail.com>
2021-02-11 13:26:33 -0600

    Accept input and debug puzzles
    *Manhattan distance and inversion

--- src/8puzzle.cpp
+++ src/8puzzle.cpp
@@ -1,9 +1,36 @@
-#include "puzzle.h"
+#include "solver.h"
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    Puzzle p("31246587E");
-    std::cout << p.h << '\n';
+    std::ifstream inputFile;
+    inputFile.open("input.txt");
 
+    if(!inputFile) {
+        return -1;
+    }
+
+    int numPuzzles;
+    inputFile >> numPuzzles;
+    
+    for(int i = 0; i < 10; i++) {
+        std::string puzzleData = "         ";
+        std::cout << "Puzzle #" << i + 1 << ":\n";
+        for (int j = 0; j < 9; j++) {
+            inputFile >> puzzleData[j];
+            std::cout << puzzleData[j] << ' ';
+            if ((j + 1) % 3 == 0) { std::cout << '\n'; }
+        }
+        Puzzle p(puzzleData);
+        Solver s(p);
+
+        if(s.HasSolution()) std::cout << "Solveable";
+        else std::cout << "No solution";
+
+        std::cout << "\nManhattan Distance: " << p.h << "\n\n";
+    }
+
+    inputFile.close();
     return 0;
 }
